774dc0f85025de594a84c9b5bf3d5e5927f12379
abhiru-wije <abdwije19972219@gmail.com>
2021-08-31 21:41:46 +0530

    dplyr

---
+++ Data manipulation with dplyr/script.r
@@ -0,0 +1,195 @@
+#first
+counties %>%
+  # Select the columns
+  select(state, county, population, poverty)
+
+#second
+counties_selected <- counties %>%
+  select(state, county, population, private_work, public_work, self_employed)
+
+counties_selected %>%
+  # Add a verb to sort in descending order of public_work
+  arrange(desc(public_work))
+
+#third
+counties_selected <- counties %>%
+  select(state, county, population)
+
+counties_selected %>%
+  # Filter for counties with a population above 1000000
+  filter(population > 1000000)
+
+#fourth
+counties_selected <- counties %>%
+  select(state, county, population)
+
+counties_selected %>%
+  # Filter for counties with a population above 1000000
+  filter(state == "California", population > 1000000)
+
+#fifth
+counties_selected <- counties %>%
+  select(state, county, population, private_work, public_work, self_employed)
+
+# Filter for Texas and more than 10000 people; sort in descending order of private_work
+counties_selected %>%
+  # Filter for Texas and more than 10000 people
+  filter(state == "Texas", population > 10000) %>%
+  # Sort in descending order of private_work
+  arrange(desc(private_work))
+
+#sixth
+counties_selected <- counties %>%
+  select(state, county, population, public_work)
+
+counties_selected %>%
+  mutate(public_workers = public_work * population / 100) %>%
+  # Sort in descending order of the public_workers column
+  arrange(desc(public_workers))
+
+#seven
+counties_selected <- counties %>%
+  # Select the columns state, county, population, men, and women
+  select(state, county, population, men, women)
+
+counties_selected %>%
+  # Calculate proportion_women as the fraction of the population made up of women
+  mutate(proportion_women = women/population)
+
+#eight
+counties %>%
+  # Select the five columns 
+  select(state, county, population, men, women) %>%
+  # Add the proportion_men variable
+  mutate(proportion_men = men/population) %>%
+  # Filter for population of at least 10,000
+  filter(population >= 10000) %>%
+  # Arrange proportion of men in descending order 
+  arrange(desc(proportion_men))
+
+#nine
+# Use count to find the number of counties in each region
+counties_selected %>%
+  count(region, sort = TRUE) 
+
+#ten
+# Find number of counties per state, weighted by citizens, sorted in descending order
+counties_selected %>%
+  count(state, wt = citizens, sort = TRUE)
+
+#eleven
+counties_selected %>%
+  # Add population_walk containing the total number of people who walk to work 
+  mutate(population_walk = population * walk / 100) %>%
+  # Count weighted by the new column, sort in descending order
+  count(state, wt = population_walk, sort = TRUE)
+
+#twlwe
+counties_selected %>%
+  # Summarize to find minimum population, maximum unemployment, and average income
+  summarize(min_population = min(population), max_unemployment = max(unemployment), average_income = mean(income))
+
+#thirteen
+counties_selected %>%
+  group_by(state) %>%
+  summarize(total_area = sum(land_area),
+            total_population = sum(population)) %>%
+  # Add a density column
+  mutate(density = total_population/total_area) %>%
+  # Sort by density in descending order
+  arrange(desc(density))
+
+#fourteen
+counties_selected %>%
+  # Group and summarize to find the total population
+  group_by(region, state) %>%
+  summarize(total_pop = sum(population))
+
+#fifteen
+counties_selected %>%
+  # Group and summarize to find the total population
+  group_by(region, state) %>%
+  summarize(total_pop = sum(population)) %>%
+  # Calculate the average_pop and median_pop columns 
+  summarize(average_pop = mean(total_pop), median_pop = median(total_pop))
+
+#sixteen
+counties_selected %>%
+  # Group by region
+  group_by(region) %>%
+  # Find the greatest number of citizens who walk to work
+  top_n(1, walk)
+
+#seventeen
+counties_selected %>%
+  group_by(region, state) %>%
+  # Calculate average income
+  summarize(average_income = mean(income)) %>%
+  # Find the highest income state in each region
+  top_n(1, average_income)
+
+#eighteen
+counties_selected %>%
+  # Find the total population for each combination of state and metro
+  group_by(state, metro) %>%
+  summarize(total_pop = sum(population)) %>%
+  # Extract the most populated row for each state
+  top_n(1, total_pop) %>%
+  # Count the states with more people in Metro or Nonmetro areas
+  ungroup() %>% 
+  count(metro)
+
+#ninghteen
+# Glimpse the counties table
+glimpse(counties)
+
+counties %>%
+  # Select state, county, population, and industry-related columns
+  select(state, county, population, professional, service, office, construction, production) %>%
+  # Arrange service in descending order 
+  arrange(desc(service))
+
+#twenty
+counties %>%
+  # Select the state, county, population, and those ending with "work"
+  select(state, county, population, ends_with("work")) %>%
+  # Filter for counties that have at least 50% of people engaged in public work
+  filter(public_work >= 50)
+
+#twentyone
+counties %>%
+  # Count the number of counties in each state
+  count(state) %>%
+  # Rename the n column to num_counties
+  rename(num_counties = n)
+
+#twentytwo
+counties %>%
+  # Select state, county, and poverty as poverty_rate
+  select(state, county, poverty_rate = poverty)
+
+#twentythree
+counties %>%
+  # Keep the state, county, and populations columns, and add a density column
+  transmute(state, county, population, density = population / land_area) %>%
+  # Filter for counties with a population greater than one million 
+  filter(population > 1000000) %>%
+  # Sort density in ascending order 
+  arrange(density)
+
+#twentyfour
+# Change the name of the unemployment column
+counties %>%
+  rename(unemployment_rate = unemployment)
+
+# Keep the state and county columns, and the columns containing poverty
+counties %>%
+  select(state, county, contains("poverty"))
+
+# Calculate the fraction_women column without dropping the other columns
+counties %>%
+  mutate(fraction_women = women / population)
+
+# Keep only the state, county, and employment_rate columns
+counties %>%
+  transmute(state, county, employment_rate = employed / population)
