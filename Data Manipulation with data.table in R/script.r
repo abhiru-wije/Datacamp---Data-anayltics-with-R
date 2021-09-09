f7ee482c14468c11a67c32bcb8783e7ab2ea31a5
abhiru-wije <abdwije19972219@gmail.com>
2021-09-10 05:22:19 +0530

    data.table group

--- Data Manipulation with data.table in R/script.r
+++ Data Manipulation with data.table in R/script.r
@@ -130,4 +130,58 @@ duration_stats
 batrips[start_station == "Townsend at 7th" & duration < 500, hist(duration)]
 
 #26
+# Compute the mean duration for every start_station
+mean_start_stn <- batrips[, .(mean_duration = mean(duration)), by = start_station]
+mean_start_stn
+
+#27
+# Compute the mean duration for every start and end station
+mean_station <- batrips[, .(mean_duration = mean(duration)), by = .(start_station, end_station)]
+mean_station
+
+#28
+# Compute the mean duration grouped by start_station and month
+mean_start_station <- batrips[, .(mean_duration = mean(duration)), by = .(start_station, month(start_date))]
+mean_start_station
+
+#29
+# Compute mean of duration and total trips grouped by start and end stations
+aggregate_mean_trips <- batrips[, .(mean_duration = mean(duration), 
+                                    total_trips = .N), 
+                                by = .(start_station, end_station)]
+aggregate_mean_trips
+
+#30
+# Compute min and max duration grouped by start station, end station, and month
+aggregate_min_max <- batrips[, .(min_duration = min(duration), 
+                                 max_duration = max(duration)), 
+                             by = .(start_station, end_station, 
+                                    month(start_date))]
+aggregate_min_max
+
+#31
+# Arrange the total trips grouped by start_station and end_station in decreasing order
+trips_dec <- batrips[, .N, by = .(start_station, 
+                                  end_station)][order(-N)]
+trips_dec
+
+#32
+# Top five most popular destinations
+top_5 <- batrips[, .N, by = .(end_station)][order(-N)][1:5]
+top_5
+
+#33
+# Compute most popular end station for every start station
+popular_end_station <- trips_dec[, .(end_station = end_station[1]), 
+                                 by = .(start_station)]
+popular_end_station
+
+#34
+# Find the first and last ride for each start_station
+first_last <- batrips[order(start_date), 
+                      .(start_date = start_date[c(1, .N)]), 
+                      by = start_station]
+first_last
+
+#35
 
