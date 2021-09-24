cf6a0241d43d98dd399e7cd400706e76c038f40d
abhiru-wije <abdwije19972219@gmail.com>
2021-09-25 03:41:24 +0530

    completed data joining

--- Joining data with data.table in R/script.r
+++ Joining data with data.table in R/script.r
@@ -185,3 +185,130 @@ imdb[netflix_dt, on = .(title = series)]
 
 #33
 
+# Try an inner join
+merge(heart, cardio, by = "gene", allow.cartesian = TRUE)
+
+# Filter missing values
+heart_2 <- heart[!is.na(gene)]
+cardio_2 <- cardio[!is.na(gene)]
+
+# Inner join the filtered data.tables
+merge(heart_2, cardio_2, by = "gene")
+
+#34
+# Keep only the last probe for each gene
+heart_3 <- unique(heart_2, by = "gene", fromLast = TRUE)
+cardio_3 <- unique(cardio_2, by = "gene", fromLast = TRUE)
+
+# Inner join
+reproducible <- merge(heart_3, cardio_3, by = "gene", suffixes = c(".heart", ".cardio"))
+reproducible
+
+#35
+# Concatenate case numbers from weeks 50 and 51
+rbind(ebola_W50, ebola_W51)
+
+#36
+# Modify the code
+rbind(ebola_W50, ebola_W51, ebola_W52, fill = TRUE)
+
+#37
+# Concatenate its data.tables
+gdp_all_1 <- rbindlist(gdp)
+
+#38
+# Concatenate its data.tables
+gdp_all_2 <- rbindlist(gdp, idcol = "continent")
+
+#39
+# Run this code to inspect gdp_all_2
+gdp_all_2 <- rbindlist(gdp, idcol = "continent")
+str(gdp_all_2)
+gdp_all_2[95:105]
+
+# Fix the problem
+gdp_all_3 <- rbindlist(gdp, idcol = "continent", use.names = TRUE)
+gdp_all_3
+
+#40
+# Obtain countries in both Asia and Europe
+fintersect(gdp$asia, gdp$europe)
+
+# Concatenate all data tables
+gdp_all <- rbindlist(gdp)
+
+# Find all countries that span multiple continents
+gdp_all[duplicated(gdp_all)]
+
+#41
+# Get all countries in either Asia or Europe
+funion(gdp$asia, gdp$europe)
+
+# Concatenate all data tables
+gdp_all <- rbindlist(gdp)
+
+# Print all unique countries
+unique(gdp_all)
+
+#42
+# Which countries are in Africa but not considered part of the middle east?
+fsetdiff(gdp$africa, middle_east)
+
+# Which countries are in Asia but not considered part of the middle east?
+fsetdiff(gdp$asia, middle_east)
+
+# Which countries are in Europe but not considered part of the middle east?
+fsetdiff(gdp$europe, middle_east)
+
+#43
+# Print gdp_per_capita
+gdp_per_capita
+
+# Reshape gdp_per_capita to the long format
+melt(gdp_per_capita, id.vars = "year")
+
+#44
+# Rename the new columns
+melt(gdp_per_capita, id.vars = "year", 
+     variable.name = "country", value.name = "gdp_pc")
+
+#45
+# Print ebola_wide
+ebola_wide
+
+# Stack Week_50 and Week_51
+melt(ebola_wide, measure.vars = c("Week_50", "Week_51"), 
+     variable.name = "period", value.name = "cases")
+
+#46
+# Modify the code
+melt(ebola_wide, measure.vars = c("Week_50", "Week_51"), 
+     variable.name = "period", value.name = "cases", 
+     id.vars = "Location" )
+
+#47
+# Split the population column by year
+dcast(gdp_oceania, formula = country ~ year, value.var = "population")
+
+#48
+# Split the gdp column by country
+dcast(gdp_oceania, formula = year ~ country, value.var = "gdp")
+
+#49
+# Split the gdp and population columns by year
+dcast(gdp_oceania, formula = country ~ year, value.var = c("gdp", "population"))
+
+#50
+# Reshape from wide to long format
+wide <- dcast(gdp_oceania, formula = country ~ year, value.var = c("gdp", "population"))
+
+# convert to a matrix
+as.matrix(wide, rownames = "country")
+
+#51
+# Modify your previous code
+dcast(gdp_oceania, formula =  continent + country ~ year, value.var = c("gdp", "population"))
+
+#52
+# Split gdp by industry and year
+dcast(gdp_by_industry_oceania, country ~ industry + year, value.var = "gdp")
